612bf858117d56266c10a714912eb154aee63c01
Curt Morgan <curtmorgan@ool-ad0205a4.dyn.optonline.net>
2018-11-13 16:33:12 -0500

    implemented chartist

--- src/Components/Graph/index.js
+++ src/Components/Graph/index.js
@@ -1,25 +1,41 @@
 import React from 'react'
+import Chartist from 'chartist'
+import ChartistGraph from 'react-chartist'
+import "../../../node_modules/chartist/dist/chartist.min.css"
 import './stylesheet.css'
 
 function Graph(props){
   const average = props.average.sort();
   const hoursX = [];
   const karmaY = [];
+
+  let data = {labels:[], series:[]}
+  let options = {width: 800, height: 400};
+  let type = 'Bar'
+
   average.map(hour=>{
     hoursX.push(hour[0]);
     karmaY.push(hour[1].average)
   })
-  console.log('hours: '+hoursX);
-  console.log('karma: '+karmaY);
-return(
-  <div className='graph'>
-    {average.map(hour => {
-      return(
-        <p key={hour[0]}>{hour[0]}: {hour[1].average}</p>
-      )
-    })}
-  </div>
-)
+  hoursX.map(x=>{
+    data.labels.push(x);
+  })
+  data.series.push(karmaY)
+
+
+  return(
+    <div>
+    <ChartistGraph data={data} options={options} type={type} />
+    </div>
+  )
+
 }
 
 export default Graph;
+
+
+// {average.map(hour => {
+//   return(
+//     <p key={hour[0]}>{hour[0]}: {hour[1].average}</p>
+//   )
+// })}
